546f8be9bec025cafbe1186ec3632d34aa8392b5
hailDev <croziputra@gmail.com>
2025-07-15 11:09:51 +0700

    update application module

--- internal/models/application.go
+++ internal/models/application.go
@@ -1,13 +1,17 @@
 package models
 
+import (
+	"time"
+)
+
 type Application struct {
 	ID             uint   `gorm:"primaryKey"`
 	ResearchCaseID string `gorm:"not null"`
 	UserID         string `gorm:"not null"`
 	Status         string `gorm:"default:'pending'"`
-	AppliedAt      int64  `gorm:"autoCreateTime"`
-	ProcessedAt    int64  `gorm:"autoUpdateTime"`
-	ProcessedBy    string `gorm:"not null"`
+	AppliedAt      time.Time  `gorm:"autoCreateTime" json:"applied_at"`
+	ProcessedAt time.Time `gorm:"autoUpdateTime" json:"processed_at"`
+	ProcessedBy    string `gorm:"default:null"`
 
 	ResearchCase ResearchCase `gorm:"foreignKey:ResearchCaseID;constraint:OnDelete:CASCADE;"`
 	User         User         `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;"`
--- internal/repository/application_repository.go
+++ internal/repository/application_repository.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"Skripsigma-BE/internal/models"
 
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -11,7 +13,12 @@ type ApplicationRepository interface {
 	GetByID(id string) (*models.Application, error)
 	Update(application *models.Application) error
 	// GetAll() ([]models.Application, error)
-	// GetByStudentID(studentID string) (*models.Application, error)
+	GetByStudentID(studentID string) (*models.Application, error)
+	GetByResearchCaseID(researchCaseID string) ([]models.Application, error)
+	GetByUserIDAndResearchCaseID(userID, researchCaseID string) (*models.Application, error)
+	GetAllByStudentID(studentID string) ([]models.Application, error)
+	AssociateTags(researchCaseID string, tagIDs []string) error
+	CancelOtherApplications(currentAppID uint, userID string) error
 }
 
 type applicationRepository struct {
@@ -25,6 +32,28 @@ func NewApplicationRepository(db *gorm.DB) ApplicationRepository {
 func (r *applicationRepository) Create(application *models.Application) error {
 	return r.db.Create(application).Error
 }
+
+// cek apabila user sudah apply
+func (r *applicationRepository) HasApplied(userID, researchCaseID string) (bool, error) {
+	var count int64
+	err := r.db.Model(&models.Application{}).
+		Where("user_id = ? AND research_case_id = ?", userID, researchCaseID).
+		Count(&count).Error
+	if err != nil {
+		return false, err
+	}
+	return count > 0, nil
+}
+
+func (r *applicationRepository) GetByUserIDAndResearchCaseID(userID, researchCaseID string) (*models.Application, error) {
+	var application models.Application
+	err := r.db.Where("user_id = ? AND research_case_id = ?", userID, researchCaseID).First(&application).Error
+	if err != nil {
+		return nil, err
+	}
+	return &application, nil
+}
+
 
 // func (r *applicationRepository) GetAll() ([]models.Application, error) {
 	
@@ -47,3 +76,67 @@ func (r *applicationRepository) GetByID(id string) (*models.Application, error) 
 func (r *applicationRepository) Update(application *models.Application) error {
 	return r.db.Save(application).Error
 }
+
+func (r *applicationRepository) GetByStudentID(studentID string) (*models.Application, error) {
+	var application models.Application
+	if err := r.db.Where("user_id = ?", studentID).First(&application).Error; err != nil {
+		return nil, err
+	}
+	return &application, nil
+}
+
+func (r *applicationRepository) GetAllByStudentID(studentID string) ([]models.Application, error) {
+	var applications []models.Application
+	err := r.db.
+		Preload("ResearchCase").
+		Preload("ResearchCase.Company").
+		// .Preload("ResearchCase.Tags"). // kalau ingin menyertakan tags
+		Where("user_id = ?", studentID).
+		Find(&applications).Error
+
+	if err != nil {
+		return nil, err
+	}
+	return applications, nil
+}
+
+
+// get by research case id
+func (r *applicationRepository) GetByResearchCaseID(researchCaseID string) ([]models.Application, error) {
+	var applications []models.Application
+	err := r.db.
+		Preload("User").
+		Preload("ResearchCase").
+		Where("research_case_id = ?", researchCaseID).
+		Find(&applications).Error
+
+	if err != nil {
+		return nil, err
+	}
+	return applications, nil
+}
+
+func (r *applicationRepository) AssociateTags(researchCaseID string, tagIDs []string) error {
+	for _, tagID := range tagIDs {
+		researchCaseTag := models.ResearchCaseTag{
+			ResearchCaseID: researchCaseID,
+			TagID:          tagID,
+		}
+		if err := r.db.Create(&researchCaseTag).Error; err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *applicationRepository) CancelOtherApplications(currentAppID uint, userID string) error {
+	return r.db.
+		Model(&models.Application{}).
+		Where("id != ? AND user_id = ? AND status IN ?", currentAppID, userID, []string{"pending", "accepted"}).
+		Updates(map[string]interface{}{
+			"status":       "cancelled",
+			"processed_at": time.Now(),
+		}).Error
+}
+
+
